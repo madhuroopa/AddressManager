dc907ffb5378913f450cb1a03e3ec0b15230d5f0
madhuroopa <madhuroopa246@gmail.com>
2023-02-25 13:32:22 -0800

    Made changes in the script file and routes file

--- address_Manager_app/address_Manager_app/static/scripts/script.js
+++ address_Manager_app/address_Manager_app/static/scripts/script.js
@@ -1,30 +1,42 @@
 $(document).ready(function () {
-    //making an ajax request to retrieve countries
-    $.getJSON("/address_API/countries", function (data) {
-        $.each(data, function (key, country) {
-            $("#dropdown_country").append("<option value='" + country.Country_Code + "'>" + country.Country + "</option>");
+
+  $(function () {
+    // Auto-populate address list based on filter values
+    $('#streetNumber, #streetAddress, #city, #state, #postalCode, #country').on('input', function () {
+      var filters = {
+        NUMBER: $('#streetNumber').val(),
+        STREET: $('#streetAddress').val(),
+        CITY: $('#city').val(),
+        STATE: $('#state').val(),
+        POSTCODE: $('#postalCode').val(),
+        COUNTRY: $('#country').val()
+      };
+      $.getJSON('/address_API/search', filters, function (data) {
+        console.log('Received response:', data);
+        var addressList = $('#address-list');
+        addressList.empty(); // clear the address list before adding new items
+        $.each(data, function (key, value) {
+          // loop through the response data and append each item to the address list
+          $('#address-list').append('<option value="' + value.STREET +'" data-NUMBER="' + value.NUMBER + '" data-STREET="' + value.STREET + '" data-CITY="' + value.CITY + '" data-STATE="' + value.STATE + '" data-POSTCODE="' + value.POSTCODE + '" data-COUNTRY="' + value.COUNTRY + '">' + value.NUMBER + ', ' + value.STREET + ', ' + value.CITY + ', ' + value.STATE + ' ' + value.POSTCODE + ', ' + value.COUNTRY + '</option>');
         });
+      });
     });
-    $("#dropdown_country").change(function () {
-        var countryCode = $(this).val();
-        if (countryCode === "") {
-            $("#country").val("");
-            $("#country").prop("disabled", true);
-        } else {
-            // Make an AJAX request to retrieve the country name based on the country code
-            $.getJSON("/address_API/countries", function (data) {
-                var countryName = "";
-                $.each(data, function (key, country) {
-                    if (country.Country_Code === countryCode) {
-                        countryName = country.Country;
-                        return false;
-                    }
-                });
-                $("#country").val(countryName);
-                $("#country").prop("disabled", false);
-            });
-        }
+  
+    // Auto-populate other fields based on the selected option in the dropdown
+    $('#streetNumber').on('input', function () {
+      console.log('Address list change event triggered');
+      var selectedOption = $('#address-list option[value="' + $(this).val() + '"]');
+      if ($(this).val() === '') { // if street number is empty, clear other fields
+        $('#streetAddress, #city, #state, #postalCode, #dropdown_country').val('');
+      } else if (selectedOption.length > 0) {
+        $('#streetNumber').val(selectedOption.attr('data-NUMBER'));
+        $('#streetAddress').val(selectedOption.attr('data-STREET'));
+        $('#city').val(selectedOption.attr('data-CITY'));
+        $('#state').val(selectedOption.attr('data-STATE'));
+        $('#postalCode').val(selectedOption.attr('data-POSTCODE'));
+
+        $('#dropdown_country').val(selectedOption.attr('data-COUNTRY'))
+      }
     });
+  });
 });
-
-
