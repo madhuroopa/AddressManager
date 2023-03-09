1184d18a1e4163adc48bd7d8d71d077370c5a512
madhuroopa <madhuroopa246@gmail.com>
2023-03-09 12:18:49 -0800

    Made changes in script.js to handle multiple country address templates

--- address_Manager_app/address_Manager_app/main/routes.py
+++ address_Manager_app/address_Manager_app/main/routes.py
@@ -1,46 +1,77 @@
-from flask import Blueprint, render_template,jsonify, request
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Blueprint, render_template,jsonify, request, current_app
 
 from address_Manager_app.extensions import mongodb
 main =Blueprint('main',__name__)
- 
- 
+
+log_handler = RotatingFileHandler('api.log', maxBytes=100000, backupCount=20)
+log_handler.setLevel(logging.INFO)
+
+@main.before_app_first_request
+def setup_logging():
+    current_app.logger.addHandler(log_handler)
+    
+
 @main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/address_API/countries',methods=['GET'])
-def get_Countries():
-    print(mongodb.db.list_collections)
-    countries_collection = mongodb.db.Countries
-    print(countries_collection)
-    countries = list(countries_collection.find({},{"_id":0}))
-    
-    return jsonify(countries)
-@main.route('/address_API/search',methods=['GET'])
-def get_address():
-    filters = {}
+
+@main.before_request
+def log_request_info():
+    with current_app.app_context():
+        current_app.logger.info('Request URL: %s', request.url)
+        current_app.logger.info('Request method: %s', request.method)
+        current_app.logger.info('Request data : %s', request.args)
+        
+@main.route('/address_API/search', methods=['GET'])
+def search_address():
     print("hello")
-    #print(request.args.items())
-    #for key, value in request.args.items():
-    #    filters[key] = value
-    for key in ['NUMBER', 'STREET', 'CITY', 'STATE', 'POSTCODE', 'COUNTRY']:
-        value = request.args.get(key)
-        if value:
-            filters[key] = value
-    address_collection=mongodb.db.address_collection
+    
+    country_code =  json.loads(request.args.get('country_code'))
+    recipient_name = request.args.get('recipient_name')
+    street = request.args.get('street')
+    city = request.args.get('city')
+    state = request.args.get('state')
+    postal_code = request.args.get('postal_code')
+    print(country_code)
+    filters = {'country_code': {'$in': country_code}}
+    
+    if recipient_name:
+        filters['recipient_name'] = {'$regex': f'.*{recipient_name}.*', '$options': 'i'}
+    if street:
+        filters['street'] = {'$regex': f'.*{street}.*', '$options': 'i'}
+    if city:
+        filters['city'] = {'$regex': f'.*{city}.*', '$options': 'i'}
+    if state:
+        filters['state'] = {'$regex': f'.*{state}.*', '$options': 'i'}
+    if postal_code:
+        filters['postal_code'] = {'$regex': f'.*{postal_code}.*', '$options': 'i'}
     print(filters)
-    results = address_collection.find(filters)
-    print(results)
-    addresses = []
-    for result in results:
-        address = {
-            "NUMBER":result.get("NUMBER"),
-            "STREET": result.get("STREET"),
-            "CITY": result.get("CITY"),
-            "STATE": result.get("STATE"),
-            "POSTCODE": result.get("POSTCODE"),
-            "COUNTRY": result.get("COUNTRY")
-        }
-        addresses.append(address)
+    addresses = list(mongodb.db.address_collection.find(filters))
     print(addresses)
-    return jsonify(addresses)
+    for address in addresses:
+        address['_id'] = str(address['_id']) 
+    if len(addresses) == 0:
+        # return 404 Not Found status code and error message
+        return jsonify({'error': 'No addresses found for the given criteria.'}), 404
+    return jsonify(addresses),200
+@main.route('/address_API/addressformat/<country_code>')
+def get_address_format(country_code):
+    
+    addressformat = mongodb.db.address_formats.find_one({'country_code': country_code})
+    print(f"hello{addressformat}")
+    format=addressformat['address_format']
+    print(format)
+   
+    return jsonify(format)
+
+@main.after_request
+def log_response_info(response):
+    with current_app.app_context():
+        current_app.logger.info('Response status: %s', response.status)
+       # current_app.logger.info('Response headers: %s', response.headers)
+        current_app.logger.info('Response data: %s', response.get_data())
+    return response
--- address_Manager_app/address_Manager_app/static/scripts/script.js
+++ address_Manager_app/address_Manager_app/static/scripts/script.js
@@ -1,28 +1,277 @@
 $(document).ready(function () {
-
-  $(function () {
-    // Auto-populate address list based on filter values
-    $('#streetNumber, #streetAddress, #city, #state, #postalCode, #country').on('input', function () {
-      var filters = {
-        NUMBER: $('#streetNumber').val(),
-        STREET: $('#streetAddress').val(),
-        CITY: $('#city').val(),
-        STATE: $('#state').val(),
-        POSTCODE: $('#postalCode').val(),
-        COUNTRY: $('#country').val()
-      };
-      $.getJSON('/address_API/search', filters, function (data) {
-        console.log('Received response:', data);
-        var addressList = $('#address-list');
-        addressList.empty(); // clear the address list before adding new items
-        $.each(data, function (key, value) {
-          // loop through the response data and append each item to the address list
-          $('#address-list').append('<option value="' + value.STREET +'" data-NUMBER="' + value.NUMBER + '" data-STREET="' + value.STREET + '" data-CITY="' + value.CITY + '" data-STATE="' + value.STATE + '" data-POSTCODE="' + value.POSTCODE + '" data-COUNTRY="' + value.COUNTRY + '">' + value.NUMBER + ', ' + value.STREET + ', ' + value.CITY + ', ' + value.STATE + ' ' + value.POSTCODE + ', ' + value.COUNTRY + '</option>');
-        });
-      });
+    document.getElementById('form').style.display="none";
+    var table = $('#addresses');
+    var fields=[]
+    $('#country_codes').on('change', function() {
+      var countryCode = $(this).val();
+      $("#address-search-form input[type='text'], #address-search-form select").val('');
+      table.empty();
+      fields=[]
+      updateAddressForm(countryCode);
+      
     });
-  
-    // Auto-populate other fields based on the selected option in the dropdown
+    function updateAddressForm(countryCodes) {
+      if (countryCodes.length > 1) {
+        $('.form-group').removeClass('hidden');
+        $(".form-group").each(function() {
+          // Get the input element and its label
+          var input = $(this).find(".form-control");
+          var label = $(this).find(".label");
+        
+          // Reset the label and placeholder values to the default values
+          label.text(input.attr("data-default-label"));
+          
+          input.attr("placeholder", input.attr("data-default-placeholder"));
+        });
+     
+        document.getElementById('form').style.display = "block";
+        
+        // Display the country agnostic form that includes all the necessary fields for all the selected countries
+      }
+      else{
+        var countryCode = countryCodes[0];
+        $.ajax({
+          url: '/address_API/addressformat/' + countryCode,
+          success: function(data) {
+            //console.log(data)
+            length=data.length
+            console.log("hello")
+            console.log(data)
+            var visibleDivIds = [];
+            
+             for (var i = 0; i < length; i++) {
+              var component = Object.keys(data[i])[0];
+              console.log("hello")
+              console.log(data[i][component][0])
+              console.log(component)
+              var input = $('#' + component);
+              
+              
+              if (input.length > 0) {
+                if(data[i][component][0]!=""){
+                  fields.push(component)
+                  input.attr('placeholder', data[i][component][1]);
+                  input.prev().text(data[i][component][0].trim());
+                  var divId = $('#' + component).closest("div").attr("id");
+                  console.log(divId)
+                  
+                 // document.getElementById('street_address').style.display="block"
+                  var divElement = document.getElementById(divId);
+                  if (divElement) {
+                    divElement.classList.remove('hidden');
+                    visibleDivIds.push(divId);
+                    } 
+                }
+                else{
+                  console.log("hello")
+                  var divId = $('#' + component).closest("div").attr("id");
+                  console.log(divId)
+                  
+                 // document.getElementById('street_address').style.display="block"
+                  var divElement = document.getElementById(divId);
+                  if (divElement) {
+                    divElement.classList.add('hidden');
+                    } 
+
+                }
+
+            }
+          }
+         
+        $('.form-group').each(function() {
+          if (visibleDivIds.indexOf($(this).attr('id')) == -1) {
+            $(this).addClass('hidden');
+          } else {
+            $(this).removeClass('hidden');
+          }
+
+        });
+        document.getElementById('form').style.display = "block";
+        document.getElementById('button').style.display = "block";
+        //document.getElementById('region_class').style.display = "block";
+
+          /*  if(flag){
+             document.getElementById('form').style.display="block"
+             
+            }
+            else{
+              document.getElementById('form').style.display="block"
+              document.getElementById('region_class').style.display="none"
+
+            }*/
+
+            }
+        });
+     }
+  };
+
+      
+    
+    
+    $('#address-search-form').submit(function(e) {
+        e.preventDefault(); // prevent the form from submitting normally
+
+        // get the form values
+        var recipient_name = $('#recipient_name').val();
+        var street = $('#street').val();
+        var city = $('#city').val();
+        var state = $('#state').val();
+        var postal_code = $('#postal_code').val();
+        var country_code = $('#country_codes').val().map(function(value) {
+          return value.replace(/"/g, '');
+        });
+        console.log($('#country_codes').val())
+        // construct the query parameters
+        var params = {
+          'recipient_name': recipient_name,
+          'street': street,
+          'city': city,
+          'state': state,
+          'postal_code': postal_code,
+          'country_code': JSON.stringify(country_code)
+        };
+        
+        console.log(params)
+       
+        // make an AJAX request to the API endpoint
+        $('#alert-container').empty();
+        $('#addresses').empty();
+        $.getJSON('/address_API/search', params)
+        .done(function (data) {
+          
+          console.log('Received response:', data);
+          
+                // handle the response data
+                
+                var addresses = data;
+                
+                console.log(fields)
+                table.empty();
+                var headerRow = $("<tr>");
+              if(fields.length!=0){
+                for (i=0;i<fields.length;i++){
+                  var input = $('#' +fields[i]);
+                  label=input.prev().text();
+                  headerRow.append($("<th>").text(label));
+                }
+                table.append(headerRow);
+              }
+              else{
+                var headerRow = $("<tr>");
+                headerRow.append($("<th>").text("Country"));
+                headerRow.append($("<th>").text("Country Name"));
+                headerRow.append($("<th>").text("Recipient Name"));
+                headerRow.append($("<th>").text("Street"));
+                headerRow.append($("<th>").text("Postal Code"));
+                headerRow.append($("<th>").text("City"));
+                headerRow.append($("<th>").text("State"));
+                table.append(headerRow);
+              }
+                
+                
+               
+              if(fields.length!=0){
+                $.each(addresses, function(index, address) {
+                    var row = $("<tr>");
+                 
+                    for (i=0;i<fields.length;i++){
+                      if(fields[i]=="recipient_name"){
+                        row.append($("<td>").text(address.recipient_name));
+                      }
+                      if(fields[i]=="postal_code"){
+                        row.append($("<td>").text(address.postal_code));
+                      }
+                      if(fields[i]=="region"){
+                        row.append($("<td>").text(address.region));
+                      }
+                     
+                      if(fields[i]=="street"){
+                        row.append($("<td>").text(address.street));
+                      }
+                      if(fields[i]=="city"){
+                        row.append($("<td>").text(address.city));
+                      }
+                      if(fields[i]=="state"){
+                        row.append($("<td>").text(address.state));
+                      }
+                     
+                      if(fields[i]=="country_codes"){
+                        row.append($("<td>").text(address.country));
+                      }
+                    table.append(row);
+                    console.log(row)
+                    
+                    }
+                  });
+                  }
+                    else{
+                      $.each(addresses, function(index, address) {
+                        var row = $("<tr>");
+                        row.append($("<td>").text(address.country_code));
+                        row.append($("<td>").text(address.country));
+                        row.append($("<td>").text(address.recipient_name));
+                        row.append($("<td>").text(address.street));
+                        row.append($("<td>").text(address.postal_code));
+                        row.append($("<td>").text(address.city));
+                        row.append($("<td>").text(address.state));
+                        table.append(row);
+    
+
+                      });
+                    }
+               
+              })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+          if (jqXHR.status === 404) {
+            // handle 404 error
+            
+              // show a Bootstrap error alert
+              var alert = $('<div class="alert alert-danger alert-dismissible fade show" role="alert"> <div>No addresses found for the given criteria.</div></div>');
+              $('#alert-container').empty().append(alert);
+              var svg = $()
+              return;
+          
+          } 
+        
+          else if (jqXHR.status === 500) {
+            // handle 500 error
+            var alert = $('<div class="alert alert-danger alert-dismissible fade show" role="alert"> <div>500: Internal Servor Error</div></div>');
+            $('#alert-container').empty().append(alert);
+            var svg = $()
+            return;
+          } 
+        });
+        
+    });
+    $('#addresses').on("click","tr",function () {
+      if(fields.length!=0){
+      for (i=0; i<fields.length;i++){
+        var input = $('#' +fields[i]);
+        var value = $(this).find("td:nth-child(" + (i+1) + ")").text();
+
+        input.val(value);
+
+      }
+    }
+    else{
+      var country_code = $(this).find("td:nth-child(1)").text();
+      var recipient_name = $(this).find("td:nth-child(3)").text();
+      var street=$(this).find("td:nth-child(4)").text();
+      var postal_code = $(this).find("td:nth-child(5)").text();
+      var city = $(this).find("td:nth-child(6)").text();
+      var state = $(this).find("td:nth-child(7)").text();
+    $("#country_codes").val([country_code]); // use an array to set the value of a multi-select dropdown
+    $("#recipient_name").val(recipient_name);
+    $("#street").val(street);
+    $("#city").val(city);
+    $("#state").val(state);
+    $("#postal_code").val(postal_code);
+
+    }
+    });
+
+
+   /* // Auto-populate other fields based on the selected option in the dropdown
     $('#streetNumber').on('input', function () {
       console.log('Address list change event triggered');
       var selectedOption = $('#address-list option[value="' + $(this).val() + '"]');
@@ -38,5 +287,8 @@ $(document).ready(function () {
         $('#dropdown_country').val(selectedOption.attr('data-COUNTRY'))
       }
     });
+    */
   });
-});
+
+
+
